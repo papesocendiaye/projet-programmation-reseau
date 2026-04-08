3e135a0f7a6d140fc1df34f44611850a08895e1f
Chahd2005 <chahd.ben_aissa@insa-cvl.fr>
2026-04-08 16:44:05 +0200

    Merge branch 'main' of https://github.com/papesocendiaye/projet-programmation-reseau
    modification p2p main.c

--- partie_c/main.c
+++ partie_c/main.c
@@ -4,115 +4,104 @@
 #include "protocol.h"
 
 #ifdef _WIN32
-  #include <winsock2.h>
-  #include <ws2tcpip.h>
-  #pragma comment(lib, "ws2_32.lib")
-  typedef int socklen_t;
+    #include <winsock2.h>
+    #pragma comment(lib, "ws2_32.lib")
 #else
-  #include <unistd.h>
-  #include <arpa/inet.h>
-  #include <sys/socket.h>
-  #define INVALID_SOCKET -1
-  #define SOCKET_ERROR   -1
-  typedef int SOCKET;
+    #include <unistd.h>
+    #include <arpa/inet.h>
+    #include <sys/select.h>
 #endif
 
-#define PORT 5000
+#define PORT_IA 5000      // Port pour parler au Python local
+#define PORT_RESEAU 6000  // Port pour parler aux autres joueurs
+#define MAX_PEERS 10
 
-static void socket_close(SOCKET s) {
-#ifdef _WIN32
-    closesocket(s);
-#else
-    close(s);
-#endif
+typedef struct {
+    struct sockaddr_in addr;
+    int active;
+} Peer;
+
+Peer lobby[MAX_PEERS]; // Carnet d'adresses
+
+// Ajoute une IP au carnet si elle n'y est pas déjà
+void add_peer(struct sockaddr_in new_addr) {
+    for (int i = 0; i < MAX_PEERS; i++) {
+        if (lobby[i].active && lobby[i].addr.sin_addr.s_addr == new_addr.sin_addr.s_addr) return;
+        if (!lobby[i].active) {
+            lobby[i].addr = new_addr;
+            lobby[i].active = 1;
+            printf("[P2P] Nouveau voisin ajouté : %s\n", inet_ntoa(new_addr.sin_addr));
+            return;
+        }
+    }
 }
 
 int main(int argc, char *argv[]) {
 #ifdef _WIN32
-    WSADATA wsaData;
-    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
-        printf("WSAStartup failed\n");
-        return 1;
-    }
+    WSADATA wsaData; WSAStartup(MAKEWORD(2, 2), &wsaData);
 #endif
 
-    SOCKET sock = socket(AF_INET, SOCK_DGRAM, 0);
-    if (sock == INVALID_SOCKET) {
-        printf("socket() failed\n");
-        return 1;
-    }
+    int sock_ia = socket(AF_INET, SOCK_DGRAM, 0);
+    int sock_reseau = socket(AF_INET, SOCK_DGRAM, 0);
 
-    struct sockaddr_in my_addr;
-    memset(&my_addr, 0, sizeof(my_addr));
-    my_addr.sin_family = AF_INET;
-    my_addr.sin_port = htons(PORT);
-
+    struct sockaddr_in addr_ia, addr_res, sender_addr;
+    socklen_t addr_len = sizeof(sender_addr);
     char buffer[MAX_BUFFER_SIZE];
 
+    // Liaison socket locale (IA)
+    addr_ia.sin_family = AF_INET;
+    addr_ia.sin_addr.s_addr = inet_addr("127.0.0.1");
+    addr_ia.sin_port = htons(PORT_IA);
+    bind(sock_ia, (struct sockaddr*)&addr_ia, sizeof(addr_ia));
+
+    // Liaison socket publique (Réseau)
+    addr_res.sin_family = AF_INET;
+    addr_res.sin_addr.s_addr = INADDR_ANY;
+    addr_res.sin_port = htons(PORT_RESEAU);
+    bind(sock_reseau, (struct sockaddr*)&addr_res, sizeof(addr_res));
+
+    printf("Nœud P2P actif. IA sur 5000, Réseau sur 6000.\n");
+
+    // Si on donne une IP au lancement, on envoie un HELLO
     if (argc > 1) {
-        // --- MODE ENVOYEUR ---
-        struct sockaddr_in other_addr;
-        memset(&other_addr, 0, sizeof(other_addr));
-        other_addr.sin_family = AF_INET;
-        other_addr.sin_port = htons(PORT);
-        other_addr.sin_addr.s_addr = inet_addr(argv[1]);
+        struct sockaddr_in first_peer;
+        first_peer.sin_family = AF_INET;
+        first_peer.sin_addr.s_addr = inet_addr(argv[1]);
+        first_peer.sin_port = htons(PORT_RESEAU);
+        
+        Message h = {0, 0, 0, ACTION_HELLO, "HELLO"};
+        serialize_message(&h, buffer, sizeof(buffer));
+        sendto(sock_reseau, buffer, strlen(buffer), 0, (struct sockaddr*)&first_peer, sizeof(first_peer));
+        add_peer(first_peer);
+    }
 
-        Message msg = {1, 100, 200, ACTION_MOVE, "U37"};
-        serialize_message(&msg, buffer, sizeof(buffer));
+    fd_set readfds;
+    while (1) {
+        FD_ZERO(&readfds);
+        FD_SET(sock_ia, &readfds);
+        FD_SET(sock_reseau, &readfds);
+        int max_fd = (sock_ia > sock_reseau) ? sock_ia : sock_reseau;
 
-        // Test: envoyer 3 paquets
-        for (int i = 0; i < 3; i++) {
-            int sent = sendto(sock, buffer, (int)strlen(buffer), 0,
-                              (struct sockaddr*)&other_addr, (socklen_t)sizeof(other_addr));
-            if (sent == SOCKET_ERROR) {
-                printf("sendto failed\n");
-                break;
+        select(max_fd + 1, &readfds, NULL, NULL, NULL);
+
+        // CAS 1 : L'IA locale envoie un ordre -> On le broadcast au réseau
+        if (FD_ISSET(sock_ia, &readfds)) {
+            int len = recvfrom(sock_ia, buffer, MAX_BUFFER_SIZE-1, 0, NULL, NULL);
+            for (int i=0; i<MAX_PEERS; i++) {
+                if (lobby[i].active) sendto(sock_reseau, buffer, len, 0, (struct sockaddr*)&lobby[i].addr, sizeof(lobby[i].addr));
             }
-            printf("UDP sent %d: %s\n", i + 1, buffer);
         }
 
-    } else {
-        // --- MODE RECEPTEUR ---
-        my_addr.sin_addr.s_addr = INADDR_ANY;
-
-        if (bind(sock, (struct sockaddr*)&my_addr, (socklen_t)sizeof(my_addr)) == SOCKET_ERROR) {
-            printf("bind() failed\n");
-            socket_close(sock);
-            return 1;
-        }
-
-        printf("UDP listen on port %d...\n", PORT);
-
-        while (1) {
-            struct sockaddr_in src_addr;
-            socklen_t src_len = sizeof(src_addr);
-            int len = recvfrom(sock, buffer, MAX_BUFFER_SIZE - 1, 0,
-                               (struct sockaddr*)&src_addr, &src_len);
-            if (len <= 0) {
-                // best-effort: on continue
-                continue;
-            }
+        // CAS 2 : Message du réseau -> On l'ajoute au carnet ou on l'envoie à l'IA
+        if (FD_ISSET(sock_reseau, &readfds)) {
+            int len = recvfrom(sock_reseau, buffer, MAX_BUFFER_SIZE-1, 0, (struct sockaddr*)&sender_addr, &addr_len);
             buffer[len] = '\0';
-
-            Message received_msg;
-            if (deserialize_message(buffer, &received_msg)) {
-                printf("[UDP RECV] from %s | player=%d action=%d pos=(%d,%d) target=%s\n",
-                       inet_ntoa(src_addr.sin_addr),
-                       received_msg.id_joueur,
-                       (int)received_msg.action,
-                       received_msg.pos_x,
-                       received_msg.pos_y,
-                       received_msg.target_id);
-            } else {
-                printf("[UDP RECV] invalid packet: %s\n", buffer);
+            Message m;
+            if (deserialize_message(buffer, &m)) {
+                if (m.action == ACTION_HELLO) add_peer(sender_addr);
+                else sendto(sock_ia, buffer, len, 0, (struct sockaddr*)&addr_ia, sizeof(addr_ia));
             }
         }
     }
-
-    socket_close(sock);
-
-#ifdef _WIN32
-    WSACleanup();
-#endif
     return 0;
 }
