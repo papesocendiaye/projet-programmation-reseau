66708a080444dde062ca26b4644f688c83866868
Jadeinsa <jade.bouayadine@insa-cvl.fr>
2026-04-08 14:11:55 +0200

    Update test_ia.py
    update de test_ia avec UDP

--- partie_c/test_ia.py
+++ partie_c/test_ia.py
@@ -1,23 +1,23 @@
 import socket
-from protocol import Message  # Message est dans protocol.py
-from tampon import TCPBuffer   # TCPBuffer est dans tampon.py
-#changement
-# On crée un serveur qui attend le programme C
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(('0.0.0.0', 5000))
-server.listen(1)
-print("IA en attente du programme C...")
+# On importe le nouveau protocole simplifié
+from protocol import Message, ActionType 
 
-conn, addr = server.accept()
-buf = TCPBuffer()
+# Configuration
+DEST_IP = "127.0.0.1"
+PORT = 5000
 
-data = conn.recv(1024)
-if data:
-    buf.add_data(data)
-    msg_str = buf.get_next_message()
-    if msg_str:
-        msg = Message.deserialize(msg_str)
-        print(f"IA a reçu : Joueur {msg.id_joueur} se déplace en ({msg.pos_x}, {msg.pos_y})")
+# Création d'un message de test
+# (Joueur 2, Unité U99, Attaque, position 450,300)
+msg = Message(
+    id_joueur=2, 
+    pos_x=450, 
+    pos_y=300, 
+    action=ActionType.ATTACK, 
+    target_id="U99"
+)
 
-conn.close()
-server.close()
+# Envoi via UDP
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.sendto(msg.serialize(), (DEST_IP, PORT))
+
+print(f"Message envoyé : {msg.target_id} attaque !")
