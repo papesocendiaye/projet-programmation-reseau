e4751e209ece1cd2fe36d5f5664212cf97fd7c13
RachkidK <nouhourachkid@gmail.com>
2026-04-09 09:04:52 +0200

    mis à jour

---
+++ partie_c/joueur_bleu.py
@@ -0,0 +1,12 @@
+from engine import Engine
+
+if __name__ == "__main__":
+    print("--- Démarrage Joueur BLEU ---")
+    moteur = Engine(
+        scenario="lanchester", # <-- DOIT ETRE LE MÊME SCENARIO
+        ia1="random_ia",       # <-- DOIT ETRE LA MÊME IA
+        ia2="random_ia",       # <-- DOIT ETRE LA MÊME IA
+        view_type=2, 
+        local_team='B'         # <-- Ce PC calcule l'équipe Bleue
+    )
+    moteur.start()
---
+++ partie_c/joueur_rouge.py
@@ -0,0 +1,14 @@
+from engine import Engine
+
+# Remplacez "nom_du_scenario", "nom_ia_1" et "nom_ia_2" par les VRAIS noms 
+# qui sont dans votre AI_REGISTRY et vos fichiers de scénarios.
+if __name__ == "__main__":
+    print("--- Démarrage Joueur ROUGE ---")
+    moteur = Engine(
+        scenario="lanchester", # <-- METTEZ VOTRE SCENARIO ICI
+        ia1="random_ia",       # <-- METTEZ VOTRE IA ROUGE ICI
+        ia2="random_ia",       # <-- METTEZ VOTRE IA BLEUE ICI
+        view_type=2, 
+        local_team='R'         # <-- Ce PC calcule l'équipe Rouge
+    )
+    moteur.start()
