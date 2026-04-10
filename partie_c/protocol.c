287a6dcf5b36392010058726b21ee695568d9d09
Chahd2005 <chahd.ben_aissa@insa-cvl.fr>
2026-04-10 09:51:47 +0200

    Update protocol.c
    v2 protocol.c

--- partie_c/protocol.c
+++ partie_c/protocol.c
@@ -1,12 +1,12 @@
 #include "protocol.h"
+#include <string.h>
 
-void serialize_message(const Message* msg, char* buffer, size_t buffer_size) {
-    snprintf(buffer, buffer_size, "%d|%d|%d|%d|%s", 
-             msg->id_joueur, msg->pos_x, msg->pos_y, (int)msg->action, msg->target_id);
+void serialize_binary(const Message* msg, char* buffer) {
+    // On copie directement la structure dans le buffer réseau
+    memcpy(buffer, msg, sizeof(Message));
 }
 
-int deserialize_message(const char* str, Message* msg) {
-    int parsed = sscanf(str, "%d|%d|%d|%d|%31[^|]", 
-                        &msg->id_joueur, &msg->pos_x, &msg->pos_y, (int*)&msg->action, msg->target_id);
-    return (parsed == 5); 
+void deserialize_binary(const char* buffer, Message* msg) {
+    // On reconstruit la structure à partir des octets reçus
+    memcpy(msg, buffer, sizeof(Message));
 }
