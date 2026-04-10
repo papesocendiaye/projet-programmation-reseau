1f84e85476216082c28db4a9ad3950560f4401ca
Chahd2005 <chahd.ben_aissa@insa-cvl.fr>
2026-04-10 09:51:30 +0200

    Update main.c
    v2_modification main.c

--- partie_c/main.c
+++ partie_c/main.c
@@ -2,9 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include "protocol.h"
-#ifdef _WIN32
-    typedef int socklen_t;
-#endif
 
 #ifdef _WIN32
     #include <winsock2.h>
@@ -15,8 +12,8 @@
     #include <sys/select.h>
 #endif
 
-#define PORT_IA 5000      // Port pour parler au Python local
-#define PORT_RESEAU 6000  // Port pour parler aux autres joueurs
+#define PORT_IA 5000      // Port pour l'IA Python locale
+#define PORT_RESEAU 6000  // Port pour les autres nœuds P2P
 #define MAX_PEERS 10
 
 typedef struct {
@@ -24,9 +21,9 @@ typedef struct {
     int active;
 } Peer;
 
-Peer lobby[MAX_PEERS]; // Carnet d'adresses
+Peer lobby[MAX_PEERS]; 
 
-// Ajoute une IP au carnet si elle n'y est pas déjà
+// --- Gestion du carnet d'adresses ---
 void add_peer(struct sockaddr_in new_addr) {
     for (int i = 0; i < MAX_PEERS; i++) {
         if (lobby[i].active && lobby[i].addr.sin_addr.s_addr == new_addr.sin_addr.s_addr) return;
@@ -47,49 +44,39 @@ int main(int argc, char *argv[]) {
     int sock_ia = socket(AF_INET, SOCK_DGRAM, 0);
     int sock_reseau = socket(AF_INET, SOCK_DGRAM, 0);
 
-    struct sockaddr_in addr_ia, addr_res;
     struct sockaddr_in addr_ia, addr_res, sender_addr;
     socklen_t addr_len = sizeof(sender_addr);
-    char buffer[MAX_BUFFER_SIZE];
+    char buffer[sizeof(Message)]; // Le buffer a maintenant la taille exacte du message binaire
 
-    // Liaison socket locale (IA)
+    // Liaison socket IA (127.0.0.1:5000)
+    memset(&addr_ia, 0, sizeof(addr_ia));
     addr_ia.sin_family = AF_INET;
     addr_ia.sin_addr.s_addr = inet_addr("127.0.0.1");
     addr_ia.sin_port = htons(PORT_IA);
     bind(sock_ia, (struct sockaddr*)&addr_ia, sizeof(addr_ia));
 
-    // Liaison socket publique (Réseau)
+    // Liaison socket Réseau (0.0.0.0:6000)
+    memset(&addr_res, 0, sizeof(addr_res));
     addr_res.sin_family = AF_INET;
     addr_res.sin_addr.s_addr = INADDR_ANY;
     addr_res.sin_port = htons(PORT_RESEAU);
     bind(sock_reseau, (struct sockaddr*)&addr_res, sizeof(addr_res));
 
-    printf("Nœud P2P actif. IA sur 5000, Réseau sur 6000.\n");
+    printf("Nœud P2P Version 2 (Binaire) actif.\n");
+    printf("IA sur port %d, Réseau sur port %d\n", PORT_IA, PORT_RESEAU);
 
-    // Si on donne une IP au lancement, on envoie un HELLO
+    // Connexion initiale si IP fournie
     if (argc > 1) {
         struct sockaddr_in first_peer;
         first_peer.sin_family = AF_INET;
         first_peer.sin_addr.s_addr = inet_addr(argv[1]);
         first_peer.sin_port = htons(PORT_RESEAU);
         
-        Message h = {0, 0, 0, ACTION_HELLO, "HELLO"};
-        serialize_message(&h, buffer, sizeof(buffer));
-        sendto(sock_reseau, buffer, strlen(buffer), 0, (struct sockaddr*)&first_peer, sizeof(first_peer));
+        Message h = {0, 0, 0, ACTION_HELLO, 0.0, "HELLO"};
+        serialize_binary(&h, buffer);
+        sendto(sock_reseau, buffer, sizeof(Message), 0, (struct sockaddr*)&first_peer, sizeof(first_peer));
         add_peer(first_peer);
     }
-
-    // --- NOUVEAU : Variables pour mémoriser l'adresse du script Python ---
-    struct sockaddr_in python_client_addr;
-    int python_connected = 0;
-
-    fd_set readfds;
-    while (1) {
-        FD_ZERO(&readfds);
-        FD_SET(sock_ia, &readfds);
-        FD_SET(sock_reseau, &readfds);
-        int max_fd = (sock_ia > sock_reseau) ? sock_ia : sock_reseau;
-
 
     fd_set readfds;
     while (1) {
@@ -100,40 +87,39 @@ int main(int argc, char *argv[]) {
 
         select(max_fd + 1, &readfds, NULL, NULL, NULL);
 
-        // CAS 1 : L'IA locale envoie un ordre -> On le broadcast au réseau
+        // --- CAS 1 : L'IA locale envoie un ordre ---
         if (FD_ISSET(sock_ia, &readfds)) {
-            struct sockaddr_in sender_addr;
-            socklen_t sender_len = sizeof(sender_addr);
-            int len = recvfrom(sock_ia, buffer, MAX_BUFFER_SIZE-1, 0, (struct sockaddr*)&sender_addr, &sender_len);
-            
-            // On mémorise dynamiquement le port du Python (5001 ou 5002)
-            python_client_addr = sender_addr;
-            python_connected = 1;
+            int len = recvfrom(sock_ia, buffer, sizeof(Message), 0, NULL, NULL);
+            if (len == sizeof(Message)) {
+                Message m_ia;
+                deserialize_binary(buffer, &m_ia);
+                printf("[IA] Action: %d | Time: %f\n", m_ia.action, m_ia.timestamp);
 
-            int len = recvfrom(sock_ia, buffer, MAX_BUFFER_SIZE-1, 0, NULL, NULL);
-            for (int i=0; i<MAX_PEERS; i++) {
-                if (lobby[i].active) sendto(sock_reseau, buffer, len, 0, (struct sockaddr*)&lobby[i].addr, sizeof(lobby[i].addr));
+                // Broadcast aux voisins
+                for (int i = 0; i < MAX_PEERS; i++) {
+                    if (lobby[i].active) {
+                        sendto(sock_reseau, buffer, sizeof(Message), 0, (struct sockaddr*)&lobby[i].addr, sizeof(lobby[i].addr));
+                    }
+                }
             }
         }
 
-        // CAS 2 : Message du réseau -> On l'ajoute au carnet ou on l'envoie à l'IA
+        // --- CAS 2 : Message venant du réseau ---
         if (FD_ISSET(sock_reseau, &readfds)) {
-            struct sockaddr_in sender_addr;
-            socklen_t addr_len = sizeof(sender_addr);
-            int len = recvfrom(sock_reseau, buffer, MAX_BUFFER_SIZE-1, 0, (struct sockaddr*)&sender_addr, &addr_len);
-            buffer[len] = '\0';
-            Message m;
-            if (deserialize_message(buffer, &m)) {
-                if (m.action == ACTION_HELLO) {
+            int len = recvfrom(sock_reseau, buffer, sizeof(Message), 0, (struct sockaddr*)&sender_addr, &addr_len);
+            if (len == sizeof(Message)) {
+                Message m_res;
+                deserialize_binary(buffer, &m_res);
+
+                if (m_res.action == ACTION_HELLO) {
                     add_peer(sender_addr);
-                } else if (python_connected) {
-                    // CORRECTION : On envoie au Python, pas à nous-même !
-                    sendto(sock_ia, buffer, len, 0, (struct sockaddr*)&python_client_addr, sizeof(python_client_addr));
+                } else {
+                    // Transmettre l'action binaire à l'IA locale pour mise à jour
+                    sendto(sock_ia, buffer, sizeof(Message), 0, (struct sockaddr*)&addr_ia, sizeof(addr_ia));
                 }
-                if (m.action == ACTION_HELLO) add_peer(sender_addr);
-                else sendto(sock_ia, buffer, len, 0, (struct sockaddr*)&addr_ia, sizeof(addr_ia));
             }
         }
     }
+
     return 0;
 }
