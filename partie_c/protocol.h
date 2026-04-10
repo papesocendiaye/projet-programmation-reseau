aff9f7fab9a9afe1ea505ffd046c6a1e0550517c
Chahd2005 <chahd.ben_aissa@insa-cvl.fr>
2026-04-10 09:52:00 +0200

    Update protocol.h
    v2 protocol.h

--- partie_c/protocol.h
+++ partie_c/protocol.h
@@ -1,28 +1,30 @@
 #ifndef PROTOCOL_H
 #define PROTOCOL_H
 
-#include <stdio.h>
-#include <string.h>
+#include <stdint.h>
 
-#define MAX_BUFFER_SIZE 1024
-
+// Mise à jour des actions pour la Version 2
 typedef enum { 
-    ACTION_MOVE, 
-    ACTION_ATTACK, 
-    ACTION_SPAWN, 
-    ACTION_REQ_OWNERSHIP,
-    ACTION_HELLO 
+    ACTION_MOVE = 0, 
+    ACTION_ATTACK = 1, 
+    ACTION_SPAWN = 2, 
+    ACTION_REQ_OWNERSHIP = 3, // Demander la propriété réseau [cite: 165]
+    ACTION_ACK_OWNERSHIP = 4, // Transférer la propriété + état [cite: 166]
+    ACTION_HELLO = 5
 } ActionType;
 
+#pragma pack(push, 1) // Force l'alignement binaire strict
 typedef struct {
-    int id_joueur;
-    int pos_x;
-    int pos_y;
-    ActionType action;
-    char target_id[32]; 
+    int32_t id_joueur;
+    int32_t pos_x;
+    int32_t pos_y;
+    int32_t action;      
+    double  timestamp;   // Pour l'ordre des événements et la cohérence [cite: 80]
+    char    target_id[32]; 
 } Message;
+#pragma pack(pop)
 
-void serialize_message(const Message* msg, char* buffer, size_t buffer_size);
-int deserialize_message(const char* str, Message* msg);
+void serialize_binary(const Message* msg, char* buffer);
+void deserialize_binary(const char* buffer, Message* msg);
 
 #endif
