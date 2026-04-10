a03bd63c636c5dc5dcbdec7637918eff75658fd4
Jadeinsa <jade.bouayadine@insa-cvl.fr>
2026-04-10 10:04:02 +0200

    Update protocol.c
    ajout de propriété réseau dans protocol.c

--- partie_c/protocol.c
+++ partie_c/protocol.c
@@ -2,11 +2,9 @@
 #include <string.h>
 
 void serialize_binary(const Message* msg, char* buffer) {
-    // On copie directement la structure dans le buffer réseau
     memcpy(buffer, msg, sizeof(Message));
 }
 
 void deserialize_binary(const char* buffer, Message* msg) {
-    // On reconstruit la structure à partir des octets reçus
     memcpy(msg, buffer, sizeof(Message));
 }
