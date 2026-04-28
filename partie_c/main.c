366a8762ee876f440401d48b22ae77efdc9917f3
ibounass21 <iangillou021@gmail.com>
2026-04-28 16:31:10 +0200

    1.3

--- partie_c/main.c
+++ partie_c/main.c
@@ -4,30 +4,35 @@
 #include "protocol.h"
 
 #ifdef _WIN32
-    #include <winsock2.h>
-    typedef int socklen_t;
-    #pragma comment(lib, "ws2_32.lib")
+#include <winsock2.h>
+typedef int socklen_t;
+#pragma comment(lib, "ws2_32.lib")
 #else
-    #include <unistd.h>
-    #include <arpa/inet.h>
-    #include <sys/select.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/select.h>
 #endif
 
-#define PORT_IA 5000      
-#define PORT_RESEAU 6000  
+#define PORT_IA 5000
+#define PORT_RESEAU 6000
 #define MAX_PEERS 10
 
-typedef struct {
+typedef struct
+{
     struct sockaddr_in addr;
     int active;
 } Peer;
 
 Peer lobby[MAX_PEERS];
 
-void add_peer(struct sockaddr_in addr) {
-    for (int i = 0; i < MAX_PEERS; i++) {
-        if (lobby[i].active && lobby[i].addr.sin_addr.s_addr == addr.sin_addr.s_addr) return;
-        if (!lobby[i].active) {
+void add_peer(struct sockaddr_in addr)
+{
+    for (int i = 0; i < MAX_PEERS; i++)
+    {
+        if (lobby[i].active && lobby[i].addr.sin_addr.s_addr == addr.sin_addr.s_addr)
+            return;
+        if (!lobby[i].active)
+        {
             lobby[i].addr = addr;
             lobby[i].active = 1;
             printf("[P2P] Nouveau voisin ajouté : %s\n", inet_ntoa(addr.sin_addr));
@@ -36,16 +41,18 @@ void add_peer(struct sockaddr_in addr) {
     }
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
 // ---> L'INITIALISATION WINDOWS OUBLIÉE EST ICI <---
 #ifdef _WIN32
     WSADATA wsaData;
-    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+    {
         printf("Erreur fatale : Impossible d'initialiser Winsock.\n");
         return 1;
     }
 #endif
-// ---------------------------------------------------
+    // ---------------------------------------------------
 
     int sock_ia = socket(AF_INET, SOCK_DGRAM, 0);
     int sock_res = socket(AF_INET, SOCK_DGRAM, 0);
@@ -57,28 +64,30 @@ int main(int argc, char *argv[]) {
     addr_ia.sin_family = AF_INET;
     addr_ia.sin_port = htons(PORT_IA);
     addr_ia.sin_addr.s_addr = inet_addr("127.0.0.1");
-    bind(sock_ia, (struct sockaddr*)&addr_ia, sizeof(addr_ia));
+    bind(sock_ia, (struct sockaddr *)&addr_ia, sizeof(addr_ia));
 
     addr_res.sin_family = AF_INET;
     addr_res.sin_port = htons(PORT_RESEAU);
     addr_res.sin_addr.s_addr = htonl(INADDR_ANY);
-    bind(sock_res, (struct sockaddr*)&addr_res, sizeof(addr_res));
+    bind(sock_res, (struct sockaddr *)&addr_res, sizeof(addr_res));
 
     printf("--- Nœud V2 (Binaire + Timestamp) actif ---\n");
 
-    if (argc > 1) {
+    if (argc > 1)
+    {
         struct sockaddr_in p = {0};
         p.sin_family = AF_INET;
         p.sin_port = htons(PORT_RESEAU);
         p.sin_addr.s_addr = inet_addr(argv[1]);
         Message h = {0, 0, 0, ACTION_HELLO, 0.0, "HELLO"};
         serialize_binary(&h, buffer);
-        sendto(sock_res, buffer, sizeof(Message), 0, (struct sockaddr*)&p, sizeof(p));
+        sendto(sock_res, buffer, sizeof(Message), 0, (struct sockaddr *)&p, sizeof(p));
         add_peer(p);
     }
 
     fd_set reads;
-    while(1) {
+    while (1)
+    {
         FD_ZERO(&reads);
         FD_SET(sock_ia, &reads);
         FD_SET(sock_res, &reads);
@@ -86,37 +95,49 @@ int main(int argc, char *argv[]) {
 
         select(max_fd + 1, &reads, NULL, NULL, NULL);
 
-        if (FD_ISSET(sock_ia, &reads)) {
+        if (FD_ISSET(sock_ia, &reads))
+        {
             int len = recvfrom(sock_ia, buffer, sizeof(buffer), 0, NULL, NULL);
-            if (len == sizeof(Message)) {
+            if (len == sizeof(Message))
+            {
                 Message m;
                 deserialize_binary(buffer, &m);
                 printf("[IA LOCALE] Action: %d recue pour %s (Taille OK)\n", m.action, m.target_id);
-                for(int i=0; i<MAX_PEERS; i++) {
-                    if(lobby[i].active) 
-                        sendto(sock_res, buffer, sizeof(Message), 0, (struct sockaddr*)&lobby[i].addr, sizeof(lobby[i].addr));
+                for (int i = 0; i < MAX_PEERS; i++)
+                {
+                    if (lobby[i].active)
+                        sendto(sock_res, buffer, sizeof(Message), 0, (struct sockaddr *)&lobby[i].addr, sizeof(lobby[i].addr));
                 }
-            } else if (len > 0) {
+            }
+            else if (len > 0)
+            {
                 // ---> SÉCURITÉ : Affiche si les tailles Python/C ne matchent pas <---
                 printf("[IA LOCALE] ❌ Erreur : Paquet recu de %d octets (attendu : %zu octets)\n", len, sizeof(Message));
             }
         }
 
-        if (FD_ISSET(sock_res, &reads)) {
-            int len = recvfrom(sock_res, buffer, sizeof(buffer), 0, (struct sockaddr*)&sender_addr, &addr_len);
-            if (len == sizeof(Message)) {
+        if (FD_ISSET(sock_res, &reads))
+        {
+            int len = recvfrom(sock_res, buffer, sizeof(buffer), 0, (struct sockaddr *)&sender_addr, &addr_len);
+            if (len == sizeof(Message))
+            {
                 Message m;
                 deserialize_binary(buffer, &m);
-                if (m.action == ACTION_HELLO) {
+                if (m.action == ACTION_HELLO)
+                {
                     add_peer(sender_addr);
-                } else {
+                }
+                else
+                {
                     printf("[RESEAU] Action: %d recue | Time: %f\n", m.action, m.timestamp);
-                    sendto(sock_ia, buffer, sizeof(Message), 0, (struct sockaddr*)&addr_ia, sizeof(addr_ia));
+                    struct sockaddr_in addr_python = addr_ia;
+                    addr_python.sin_port = htons(5001); // Le vrai port d'écoute du Python !
+                    sendto(sock_ia, buffer, sizeof(Message), 0, (struct sockaddr *)&addr_python, sizeof(addr_python));
                 }
             }
         }
     }
-    
+
 #ifdef _WIN32
     WSACleanup();
 #endif
--- partie_c/protocol.c
+++ partie_c/protocol.c
@@ -1,25 +1,10 @@
 #include "protocol.h"
+#include <string.h>
 
-void serialize_message(const Message* msg, char* buffer, size_t buffer_size) {
-    snprintf(buffer, buffer_size, "%d|%d|%d|%d|%s",
-             msg->id_joueur, msg->pos_x, msg->pos_y, (int)msg->action, msg->target_id);
+void serialize_binary(const Message* msg, char* buffer) {
+    memcpy(buffer, msg, sizeof(Message));
 }
 
-int deserialize_message(const char* str, Message* msg) {
-    int action_tmp = 0;
-    char target_tmp[TARGET_ID_MAX] = {0};
-
-    int parsed = sscanf(str, "%d|%d|%d|%d|%31[^\n]",
-                        &msg->id_joueur,
-                        &msg->pos_x,
-                        &msg->pos_y,
-                        &action_tmp,
-                        target_tmp);
-
-    if (parsed != 5) return 0;
-
-    msg->action = (ActionType)action_tmp;
-    strncpy(msg->target_id, target_tmp, TARGET_ID_MAX - 1);
-    msg->target_id[TARGET_ID_MAX - 1] = '\0';
-    return 1;
+void deserialize_binary(const char* buffer, Message* msg) {
+    memcpy(msg, buffer, sizeof(Message));
 }
